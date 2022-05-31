c1738cf3a80a916b9f2b36ffeb59d6a41a936800
anant-infinity <278anant@gmail.com>
2022-06-01 00:29:59 +0530

    Added code for plotting Example DAXSS Spectra

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # Python Program to view Level-1 DAXSS netCDF files
 import netCDF4 as nc
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+
 # Path to Level-1 File
 daxss_level1_file_path = 'C:/Users/278an/Desktop/science_analysis/301_Data/level1/daxss_solarSXR_level1_2022-02-14-mission_v1.0.0.ncdf'
 #Import File as a netCDF Dataset
 daxsslevel1 = nc.Dataset(daxss_level1_file_path)
 
 #Printing the dataset to get information of metadata, dimensions and variables
-#print(daxsslevel1)
+print(daxsslevel1)
 
 #Creating a dictionary containing the information about the dataset
 metadata_info_dict = daxsslevel1.__dict__
@@ -23,22 +23,40 @@ for dim in daxsslevel1.dimensions.values():
 for var in daxsslevel1.variables.values():
     print(var)
 
-#Slow count
+plt.figure()
+#Plotting the X123 Slow Count
+plt.subplot(121)
 x123_slow_count = daxsslevel1['DATA.X123_SLOW_COUNT'][:]
-print(x123_slow_count)
-
-spectrum_index = 2913
-#fig, ax = plt.subplots(1)
-# plt.plot(daxsslevel1[spectrum_index]["energy"], daxsslevel1[spectrum_index]["irradiance"], drawstyle="steps-mid")
 plt.scatter(daxsslevel1['DATA.TIME_JD'][0,:], daxsslevel1['DATA.X123_SLOW_COUNT'][0, :], c ="blue",
             marker ="s",
             edgecolor ="blue",
-            s = 5)  # If loaded from netCDF
-#fig.autofmt_xdate()
-#ax.xaxis.set_major_formatter(mdates.DateFormatter("%b - %Y"))
+            s = 5)
+
 plt.xlabel("Time_JD")
 plt.ylim([1e3, 1e6])
 plt.yscale("log")
 plt.ylabel("DAXSS X123_Slow_Count (cps)")
-plt.suptitle("DAXSS Solar SXR Spectrum ")
+plt.suptitle("DAXSS X123 Slow Count ")
+
+#Plotting Spectrum
+plt.subplot(122)
+index_quiet_sun = 0
+irradiance_quite_sun = daxsslevel1['DATA.IRRADIANCE'][0,0,:]
+index_flare = 1381
+irradiance_flare = daxsslevel1['DATA.IRRADIANCE'][0,1381,:]
+index_eclipse = 3245
+irradiance_eclipse = daxsslevel1['DATA.IRRADIANCE'][0,3245,:]
+energy = daxsslevel1['DATA.ENERGY'][0,0,:]
+
+plt.plot(energy, irradiance_quite_sun, color='k', label='quite sun')
+plt.plot(energy, irradiance_flare, color='r', label='flare')
+plt.plot(energy, irradiance_eclipse, color='b', label='eclipse')
+
+plt.xlabel("Energy (keV)")
+plt.ylim([1e2, 1e9])
+plt.yscale("log")
+plt.ylabel("Irradiance (ph/s/cm^2/keV)")
+plt.suptitle("DAXSS Example Spectra ")
+plt.legend()
 plt.show()
+
